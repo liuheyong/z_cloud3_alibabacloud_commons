1fea923f68f7d65d7562f2c6e77ad732c8e74788
liuheyong <17682347237@163.com>
2019-06-18 09:17:28 +0800

    commit

---
+++ src/main/java/com/cloud3/commons/ZCloud3AlibabacloudCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.cloud3.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZCloud3AlibabacloudCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZCloud3AlibabacloudCommonsApplication.class, args);
+    }
+
+}
